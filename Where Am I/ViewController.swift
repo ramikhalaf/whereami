34116063e7e0a8f67fbf6c20cee9933a2831705d
Rami Khalaf <ramikhalaf@users.noreply.github.com>
2016-02-22 23:27:43 -0800

    Added location updating functionality to view controller. Updated long, lat, speed, course, and nearest address.

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var manager: CLLocationManager!
+    
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    @IBOutlet var speedLabel: UILabel!
+    @IBOutlet var altitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager();
+        manager.delegate = self;
+        manager.desiredAccuracy = kCLLocationAccuracyBest;
+        manager.requestAlwaysAuthorization();
+        manager.startUpdatingLocation();
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0];
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) -> Void in
+            if (error != nil) {
+                print(error);
+                return;
+            }
+            
+            var subThoroughfare: String = ""
+            
+            if let placemark = placemarks?[0] {
+                
+                subThoroughfare = (placemark.subThoroughfare != nil) ? placemark.subThoroughfare! : "";
+                
+                self.addressLabel.text = "\(subThoroughfare) \(placemark.thoroughfare!) \n\(placemark.subAdministrativeArea!) " +
+                    "\(placemark.postalCode!) \n\(placemark.country!)";
+            }
+        }
+        
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)";
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)";
+        self.courseLabel.text = "\(userLocation.course)";
+        self.speedLabel.text = "\(userLocation.speed)";
+        self.altitudeLabel.text = "\(userLocation.altitude)";
     }
 
     override func didReceiveMemoryWarning() {
